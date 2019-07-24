566961a5940b6174a0c6063b9ccb9bbb19574c45
Layth <laith.hadeed@gmail.com>
2019-07-24 21:18:07 +0200

    1st

---
+++ test.scala
@@ -0,0 +1,21 @@
+//import ap
+//
+object test extends App {
+  val l= List(1,2,3,4,5)
+  println(l.head)
+  println(l.tail)
+  val j = List(l.head) +:l
+  println(j)
+  def fg(ll:List[Int]):List[List[Int]] =
+  {
+    def fi(ll:List[Int],acc:List[List[Int]]) :List[List[Int]]=
+    {
+      if (ll.isEmpty) acc
+      else fi(ll.tail,  if x>y-3 List(List(ll.head))++ acc.head else List(List(ll.head))++ acc  )
+    }
+    fi(ll,List())
+  }
+
+  println(fg(l))
+
+}
