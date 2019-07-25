cd23c68aa5e26d234e79195b2002c64b3c49e115
Layth <laith.hadeed@gmail.com>
2019-07-25 22:14:18 +0200

    add new files

---
+++ splitLists.scala
@@ -0,0 +1,13 @@
+//object splitLists {
+//  val l = List(1,2,3,6,7,8,11,12,13,16)
+//  def fun(alist:List[Int]):List[List[Int]] =
+//  {
+//    def splitit(acc:List[Int],original:List[Int]): List[Int] =
+//    {
+//      if (original.isEmpty) acc
+//      else splitit( original.head +: acc , original.tail)
+//    }
+//
+//  }
+//
+//}
